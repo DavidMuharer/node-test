0423245790abfadcc4c9e5ece7217f61c915608c
Muharer, David (dm655k) <dm655k@intl.att.com>
2020-01-07 13:42:44 +0200

    spelling check

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-// in the exam you need to:
-// 1. read a list of numbers from the file "unsortedfile.txt".
-// 2. Sort the numbers (Ascending).
-// 3. write the sorted list to a new file with the name "sortedFile.txt".
-// you need to use the methods writeFileSync and readFileSync.
+// In this exam you need to:
+// 1. read a list of numbers from the given file "unsortedfile.txt".
+// 2. sort the numbers (ascending).
+// 3. write the sorted list to a new file named "sortedFile.txt".
+// You must use writeFileSync and readFileSync methods.
 
 var fs = require('fs');
 const unsortedfileName = 'unsortedfile.txt'; // file with unsorted list separated with "," 
 const sortedFileName = 'sortedFile.txt' // file name to write the sorted list separated with "," 
-
-// start the login in init function , use the methodes below
 
 init = () => {
    // add your code
