816d631a48e895473f3c0a9b7002341f7d8a9ad3
Muharer, David (dm655k) <dm655k@intl.att.com>
2020-01-07 13:19:04 +0200

    fixes

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // in the exam you need to:
-// 1. read a list of nubers from the file "unsortedfile.txt".
-// 2. ascending sort the data.
+// 1. read a list of numbers from the file "unsortedfile.txt".
+// 2. Sort the numbers (Ascending).
 // 3. write the sorted list to a new file with the name "sortedFile.txt".
-// you need to use with the methods writeFileSync, readFileSync. 
+// you need to use the methods writeFileSync and readFileSync.
 
-
-var fs = require('fs'); // include file system module
-const unsortedfileName = 'unsortedfile.txt'; // file with unsorted list seperted with "," 
-const sortedFileName = 'sortedFile.txt' // file name to write the sorted list seperted with "," 
+var fs = require('fs');
+const unsortedfileName = 'unsortedfile.txt'; // file with unsorted list separated with "," 
+const sortedFileName = 'sortedFile.txt' // file name to write the sorted list separated with "," 
 
 // start the login in init function , use the methodes below
+
 init = () => {
    // add your code
 }
